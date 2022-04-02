f6bcf1a3595874a039f8191df137b1d03054000d
Rishabh1891 <76990918+Rishabh1891@users.noreply.github.com>
2022-04-02 21:27:00 +0530

    Modern Quiz

---
+++ js/questions.js
@@ -0,0 +1,116 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Is Being HR manager Fun?",
+    answer: "Moderate",
+    options: [
+      "Yes",
+      "No",
+      "Moderate",
+      "None of Above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which one is the True Avogadro Number?",
+    answer: "6.02214076 × 10(23)",
+    options: [
+      "6.02214076 × 10(23)",
+      "6.02356076 × 10(23)",
+      "6.02674076 × 10(23)",
+      "6.02964176 × 10(23)"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who is Well known as Chhatrpati?",
+    answer: "Maharaja Shivaji",
+    options: [
+      "Maharaja Sahu Raam",
+      "Chakrvarti Chandragupt",
+      "Maharaja Shivaji",
+      "Maharana Pratap"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Where is the Statue of Unity Situated?",
+    answer: "Vadodra",
+    options: [
+      "Gujrat",
+      "Vadodra",
+      "Pune",
+      "Rajasthan"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which one doesn't belongs to the Swords of Raja Shivaji Maharaj?",
+    answer: "Veerochya",
+    options: [
+      "Tulja",
+      "Bhavani",
+      "Jagdamb",
+      "Veerochya"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    {
+    numb: 6,
+    question: "Which Division of the Indian Para SF is known as 'Scorpion'?",
+    answer: "10 Para SF",
+    options: [
+      "9 Para SF",
+      "10 Para SF",
+      "11 Para SF",
+      "12 Para SF"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Which Division of the Indian AirForce SF is dangerous?",
+    answer: "Garuda",
+    options: [
+      "Cheel",
+      "Garuda",
+      "Baaj",
+      "Giddha"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Which one doesn't belong to Allies Powers (WW2):",
+    answer: "Greenland",
+    options: [
+      "Greenland",
+      "Great Britain",
+      "The United States",
+      "Soviet Union"
+    ]
+  },
+    {
+    numb: 9,
+    question: "The Victory Chant of the Army of Chhatrpati Shivaji Maharaj was:",
+    answer: "Jai Bhavani",
+    options: [
+      "Jai Bholenath",
+      "Jai Bhavani",
+      "Jai Bajrang Bali",
+      "Ganpati Bappa Mourya"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Who is Known as the Father of Indian Navy?",
+    answer: "Chhatrpati Shivaji Maharaj",
+    options: [
+      "Chhatrpati Shivaji Maharaj",
+      "Maharaj Ram Krishan Paramohana",
+      "Balkrishna Subrmanyam Ayeppa",
+      "Singh Verma IV"
+    ]
+  },
+];
